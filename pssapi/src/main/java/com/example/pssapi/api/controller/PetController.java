02a16a580785fbb63faca8132896a4274caf8c69
p-mendoncad <pedrodiasvr17@gmail.com>
2025-05-20 08:23:20 -0300

    feat: PetController

--- pssapi/src/main/java/com/example/pssapi/api/controller/PetController.java
+++ pssapi/src/main/java/com/example/pssapi/api/controller/PetController.java
@@ -1,4 +1,32 @@
 package com.example.pssapi.api.controller;
 
+import com.example.pssapi.api.dto.PetDTO;
+import com.example.pssapi.model.entity.Cliente;
+import com.example.pssapi.model.entity.Pet;
+import com.example.pssapi.service.ClienteService;
+import com.example.pssapi.service.PetService;
+import org.modelmapper.ModelMapper;
+
+import java.util.Optional;
+
 public class PetController {
+
+    private final PetService service;
+    private final ClienteService clienteService;
+
+
+
+    public Pet converter(PetDTO dto) {
+        ModelMapper modelMapper = new ModelMapper();
+        Pet Pet = modelMapper.map(dto, Pet.class);
+        if (dto.getIdCliente() != null) {
+            Optional<Cliente> Cliente = ClienteService.getClienteById(dto.getIdCliente());
+            if (!Cliente.isPresent()) {
+                Pet.setCliente(null);
+            } else {
+                Pet.setCliente(Cliente.get());
+            }
+        }
+        return Pet;
+    }
 }
